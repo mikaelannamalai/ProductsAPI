876274778030ecc8304eb748f8954e1e5edd1d42
Mikael1287 <mikael747@gmail.com>
2022-10-13 12:06:57 -0400

    tests added

--- src/test/java/ca/vanier/AppTest.java
+++ src/test/java/ca/vanier/AppTest.java
@@ -1,5 +1,7 @@
 package ca.vanier;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -7,14 +9,39 @@ import org.junit.Test;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+public class AppTest {
+
+  /**
+   * Rigorous Test :-)
+   */
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+
+  @Test
+  public void shouldAnswerNotNull() throws Exception {
+    String args[] = new String[4];
+    args[0] = "Apple";
+    args[1] = "10";
+    args[2] = "Sparta";
+    args[3] = "save";
+    ProductsObjectManager testPd = new ProductsObjectManager();
+    String result = testPd.create(args[0], args[1], args[2]);
+
+    assertNotNull("Apple", result);
+  }
+
+  @Test
+  public void shouldAnswerTrue() throws Exception {
+    String args[] = new String[4];
+    args[0] = "Apple";
+    args[1] = "10";
+    args[2] = "Sparta";
+    args[3] = "save";
+    ProductsObjectManager testPd = new ProductsObjectManager();
+    testPd.create(args[0], args[1], args[2]);
+    String result = testPd.List.get(0).getName();
+    assertEquals("Apple", result);
+  }
 }
